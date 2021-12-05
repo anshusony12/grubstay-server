98d1ca35a9160ed0fe757291734b2c4304abe6ef
deepkumar <deepkumar95412@gmail.com>
2021-12-05 09:12:42 +0530

    Commit For: HelperException for Custom Message exception

---
+++ src/main/java/com/grubstay/server/helper/HelperException.java
@@ -0,0 +1,11 @@
+package com.grubstay.server.helper;
+
+public class HelperException extends Exception{
+    public HelperException() {
+        super();
+    }
+
+    public HelperException(String message) {
+        super(message);
+    }
+}
